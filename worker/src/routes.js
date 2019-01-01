8f8b809bf75e3d4d45342c6af86dfba8c869afc5
Justin Beckwith <justin.beckwith@gmail.com>
2018-12-31 20:49:48 -0800

    Add a traceAll endpoint

--- worker/src/routes.js
+++ worker/src/routes.js
@@ -4,8 +4,11 @@ const execa = require('execa');
 const uuid = require('uuid');
 const fs = require('fs');
 const {promisify} = require('util');
+const Queue = require('p-queue');
+const fetch = require('node-fetch');
 
 const readFile = promisify(fs.readFile);
+const workerHost = process.env.WORKER_HOST || 'http://localhost:8081';
 
 exports.trace = async (req, res) => {
   try {
@@ -21,3 +24,36 @@ exports.trace = async (req, res) => {
     res.sendStatus(500).end();
   }
 };
+
+exports.traceAll = async (req, res) => {
+  try {
+    const {name} = req.body;
+    console.log(`tracing all ${name}`);
+    const npmRes = await fetch(`https://registry.npmjs.org/${name}`);
+    const package = await npmRes.json();
+    const versions = Object.keys(package.versions);
+    console.log(versions);
+    const q = new Queue({concurrency: 1});
+    const proms = versions.map(version => {
+      return q.add(async () => {
+        try {
+          const traceRes = await fetch(`${workerHost}/trace`, {
+            method: 'post',
+            body: JSON.stringify({name, version}),
+            headers: { 'Content-Type': 'application/json' }
+          });
+          const data = await traceRes.json();
+          return data;
+        } catch (e) {
+          return null;
+        }
+      });
+    });
+    let result = await Promise.all(proms);
+    result = result.filter(x => !!x);
+    res.json(result);
+  } catch (e) {
+    console.error(e);
+    res.sendStatus(500).end();
+  }
+}
--- worker/src/server.js
+++ worker/src/server.js
@@ -4,6 +4,7 @@ const routes = require('./routes');
 const app = express();
 app.use(express.json());
 app.post('/trace', routes.trace);
+app.post('/traceAll', routes.traceAll);
 
 const port = process.env.PORT || 8081;
 app.listen(port, () => console.log(`Worker service started on ${port}`));
